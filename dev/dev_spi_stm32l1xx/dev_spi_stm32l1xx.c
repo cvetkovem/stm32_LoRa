a17a4cbe0c7a9be34e69e441d96f7b27b974a822
cvetkovem <cvetkovem@gmail.com>
2016-04-12 15:33:31 +0300

    Edit the source code SPI module

--- dev/dev_spi_stm32l1xx/dev_spi_stm32l1xx.c
+++ dev/dev_spi_stm32l1xx/dev_spi_stm32l1xx.c
@@ -2,27 +2,30 @@
 #include "gpio.h"
 #include "spi.h"
 
-/* ONLY SPI1 or SPI2 */
 void SpiInit(Spi_t *obj)
 {
+    /* Gpio settings */
     GpioInit(&obj->Mosi, PIN_ALTERNATE_FCT, PIN_PUSH_PULL, PIN_PULL_DOWN, 0);
     GpioInit(&obj->Miso, PIN_ALTERNATE_FCT, PIN_PUSH_PULL, PIN_PULL_DOWN, 0);
     GpioInit(&obj->Sclk, PIN_ALTERNATE_FCT, PIN_PUSH_PULL, PIN_PULL_DOWN, 0);
     GpioInit(&obj->Nss,  PIN_ALTERNATE_FCT, PIN_PUSH_PULL, PIN_PULL_UP,   1);
 
+    /* Gpio alternate function settings */
     ((GPIO_TypeDef *)(AHBPERIPH_BASE + 0x400*(obj->Miso.portIndex)))->AFR[(obj->Miso.pinIndex < 8)?0:1] |= 0x05 << 4*(obj->Miso.pinIndex - ((obj->Miso.pinIndex < 8)?0:8));
     ((GPIO_TypeDef *)(AHBPERIPH_BASE + 0x400*(obj->Mosi.portIndex)))->AFR[(obj->Mosi.pinIndex < 8)?0:1] |= 0x05 << 4*(obj->Mosi.pinIndex - ((obj->Mosi.pinIndex < 8)?0:8));
     ((GPIO_TypeDef *)(AHBPERIPH_BASE + 0x400*(obj->Sclk.portIndex)))->AFR[(obj->Sclk.pinIndex < 8)?0:1] |= 0x05 << 4*(obj->Sclk.pinIndex - ((obj->Sclk.pinIndex < 8)?0:8));
-    ((GPIO_TypeDef *)(AHBPERIPH_BASE + 0x400*(obj->Nss.portIndex)))->AFR[(obj->Nss.pinIndex < 8)?0:1] |= 0x05 << 4*(obj->Nss.pinIndex - ((obj->Nss.pinIndex < 8)?0:8));
+    ((GPIO_TypeDef *)(AHBPERIPH_BASE + 0x400*(obj->Nss.portIndex )))->AFR[(obj->Nss.pinIndex  < 8)?0:1] |= 0x05 << 4*(obj->Nss.pinIndex  - ((obj->Nss.pinIndex  < 8)?0:8));
 
+    /* Enable bus clock */
     if(obj->Spi == (SPI_TypeDef *)SPI1_BASE) {
         RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
+    } else if(obj->Spi == (SPI_TypeDef *)SPI2_BASE){
+        RCC->APB1ENR |= RCC_APB1ENR_SPI2EN;
+    } else {
+        RCC->APB1ENR |= RCC_APB1ENR_SPI3EN;
     }
 
-    if(obj->Spi == (SPI_TypeDef *)SPI2_BASE) {
-        RCC->APB1ENR |= RCC_APB1ENR_SPI2EN; //Тактирование модуля SPI2
-    }
-
+    /** TODO START **/
     ((SPI_TypeDef *)obj->Spi)->CR1 &= ~SPI_CR1_CPOL; //Полярность тактового сигнала 0
     ((SPI_TypeDef *)obj->Spi)->CR1 &= ~SPI_CR1_CPHA; //Фаза тактового сигнала 0
     ((SPI_TypeDef *)obj->Spi)->CR1 &= ~SPI_CR1_DFF; //8 бит данных
@@ -40,19 +43,20 @@ void SpiInit(Spi_t *obj)
     }
 
     ((SPI_TypeDef *)obj->Spi)->CR1 |= SPI_CR1_SPE; //Включаем SPI
+    /** TODO END **/
 }
 
-void SpiDeInit( Spi_t *obj )
+void SpiDeInit(Spi_t *obj)
 {
 
 }
 
-uint16_t SpiInOut( Spi_t *obj, uint16_t outData )
+uint16_t SpiInOut(Spi_t *obj, uint16_t outData)
 {
     while(!(((SPI_TypeDef *)(obj->Spi))->SR & SPI_SR_TXE));
-    ((SPI_TypeDef *)(obj->Spi))->DR = outData; //Пишем в буфер передатчика SPI
-    while(!(((SPI_TypeDef *)(obj->Spi))->SR & SPI_SR_RXNE)); //Ожидаем окончания приема данных модулем SPI1 (RXNE =1 - приемный буфер содержит данные)
-    return ((SPI_TypeDef *)(obj->Spi))->DR; //Считываем данные из приемного буфера SPI При этой операции происходит очистка буфера и сброс флага RXNE
+    ((SPI_TypeDef *)(obj->Spi))->DR = outData;
+    while(!(((SPI_TypeDef *)(obj->Spi))->SR & SPI_SR_RXNE));
+    return ((SPI_TypeDef *)(obj->Spi))->DR;
 }
 
 
--- dev/spi.h
+++ dev/spi.h
@@ -17,28 +17,23 @@ Maintainer: Miguel Luis and Gregory Cristian
 
 typedef struct Spi_t
 {
-    uint32_t *Spi;
+    uint32_t *Spi; //SPI1, SPI2 or SPI3
     Gpio_t Mosi;
     Gpio_t Miso;
     Gpio_t Sclk;
     Gpio_t Nss;
 
     uint8_t slave_on;
+    uint8_t bits;
+    uint8_t cpol;
+    uint8_t cpha;
+    uint32_t bit_rate;
 } Spi_t;
 
 /*!
  * \brief Initializes the SPI object and MCU peripheral
  *
- * \remark When NSS pin is software controlled set the pin name to NC otherwise
- *         set the pin name to be used.
- *
  * \param [IN] obj  SPI object
- * \param [IN] spi_name  ONLY SPI1 or SPI2
- * \param [IN] slave_on  master or slave
- * \param [IN] bits 8 for sx1276
- * \param [IN] CPOL 0 for sx1276
- * \param [IN] CPHA 0 for sx1276
- * \param [IN] BR 1MHz for sx1276
  */
 void SpiInit(Spi_t *obj);
 
