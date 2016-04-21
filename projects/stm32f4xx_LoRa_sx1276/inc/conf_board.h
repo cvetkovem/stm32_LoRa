b60996c6cd36212f31965646f77be2ac8ffa27c0
cvetkovem <cvetkovem@gmail.com>
2016-04-21 18:34:37 +0300

    add project for stm32f4xx

---
+++ projects/stm32f4xx_LoRa_sx1276/inc/conf_board.h
@@ -0,0 +1,15 @@
+#ifndef __CONF_BOARD_H_INCLUDED__
+#define __CONF_BOARD_H_INCLUDED__
+
+#include <stdlib.h>
+#include "stm32f4xx.h"
+#include "dev_cpu.h"
+#include "gpio.h"
+#include "spi.h"
+#include "dev_hw_timer.h"
+#include "timer.h"
+#include "sx1276.h"
+
+
+
+#endif /* __CONF_BOARD_H_INCLUDED__ */
---
+++ projects/stm32f4xx_LoRa_sx1276/src/main.c
@@ -0,0 +1,9 @@
+#include "conf_board.h"
+
+int main(void)
+{
+  while(1)
+  {
+
+  }
+}
