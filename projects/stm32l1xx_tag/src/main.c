369751ccff0c743272accce3cbeb1a20f98f61bb
cvetkovem <cvetkovem@gmail.com>
2017-07-31 21:37:56 +0300

    tag prj

---
+++ projects/stm32l1xx_tag/src/main.c
@@ -0,0 +1,26 @@
+/*
+**
+**                           Main.c
+**
+**
+**********************************************************************/
+/*
+   Last committed:     $Revision: 00 $
+   Last changed by:    $Author: $
+   Last changed date:  $Date:  $
+   ID:                 $Id:  $
+
+**********************************************************************/
+#include "stm32l1xx.h"
+
+int main(void)
+{
+  while(1)
+  {
+
+  }
+}
+
+void TimerIrqHandler(void) {
+
+}
